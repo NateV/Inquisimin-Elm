9160046e84ab59dc2ef4c88299c1f34f790bc751
Nate Vogel <nathan.c.vogel@gmail.com>
2023-10-14 21:12:20 -0400

    added docs to XFDF module

--- src/XFDF.elm
+++ src/XFDF.elm
@@ -1,15 +1,39 @@
-module XFDF exposing (..)
+module XFDF exposing (
+    XFDF,
+    XFDFField,
+    xfdfToString
+    )
 --| For more easily making XFDF files
 
+{-| Assist with exporting data collected in a guided interiview into XDF format, for importing into a fillable pdf. 
 
+# XFDF
+
+XFDF is an xml format for PDF Fillable form data. We can write a guided interiew that collects data and then
+downloads the data as an XFDF file. Users can import the xfdf file into a fillable pdf. 
+
+@docs XFDF, XFDFField, xfieldToString, xfdfToString
+
+
+-}
+
+{-| Type for describing an XFDF document. 
+
+-}
 type alias XFDF =
     { href : String
     , fields : List XFDFField
     }
 
+{-| A single fillable field in an XFDF document.
+
+-}
 type XFDFField = XField String String
 
 
+{-| xfdfToString uses this to write the `field` children in and XFDF file. 
+
+-}
 xfieldToString : XFDFField -> String
 xfieldToString (XField fieldName value) = String.join "\n" 
     [ "<field  name=\"" ++ fieldName ++ "\">" 
@@ -17,6 +41,11 @@ xfieldToString (XField fieldName value) = String.join "\n"
     , "</field>"
     ]
 
+{-| Render an `XFDF` value to an xml string.
+
+You might let the user download this string as a file.
+    
+-}
 xfdfToString : XFDF -> String 
 xfdfToString xfdf = String.join "\n" <|
     [ "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
